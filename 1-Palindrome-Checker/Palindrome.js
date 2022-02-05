639fc1b1e5cfc88a836865f5478996e677240ee9
efgarro <efgarro@yahoo.com>
2022-02-05 17:41:23 -0600

    Palindrome final

--- 1-Palindrome-Checker/Palindrome.js
+++ 1-Palindrome-Checker/Palindrome.js
@@ -1,6 +1,28 @@
 function palindrome(str) {
-    console.log(str);
+    let strArr = escapeRegExp(str);
+    let numIter = Math.floor(strArr.length / 2);
+    let strArrLen = strArr.length;
+    for (let i = 0; i < numIter; i++) {
+        if (strArr[i] !== strArr[strArrLen - 1 - i]) {
+            console.log("false");
+            return false;
+        }
+    }
+    console.log("true");
     return true;
 }
 
 palindrome("eye");
+
+function escapeRegExp(text) {
+    return text
+        .replace(/[-[\]{}_:\/()*+?.,\\^$|#\s]/g, "")
+        .toLowerCase()
+        .split("");
+}
+
+// palindrome("0_0 (: /- :) 0-0");
+// palindrome("My age is 0, 0 si ega ym.");
+// palindrome("A man, a plan, a canal. Panama");
+palindrome("five|_/|four");
+palindrome("not a palindrom");
